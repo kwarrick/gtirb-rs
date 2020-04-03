e35b2483705c96da1cdb566334c49f7ed080e116
Kevin Warrick <kwarrick@grammatech.com>
2020-04-02 23:46:21 -0400

    Parse IR

---
+++ src/bin/gtirb-reader.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+
+pub fn main() -> Result<()> {
+    println!("gtirb");
+    let path = "/home/kwarrick/Linux/out.ir";
+    let ir = gtirb::read(path)?;
+    eprintln!("{}", ir.version());
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-pub mod proto {
+use std::path::Path;
+
+use anyhow::Result;
+use prost::Message;
+
+mod proto {
     include!(concat!(env!("OUT_DIR"), "/proto.rs"));
+}
+
+pub struct IR {
+    inner: proto::Ir,
+}
+
+impl IR {
+    pub fn load_protobuf<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let bytes = std::fs::read(path)?;
+        Ok(IR { inner: proto::Ir::decode(&*bytes)? })
+    }
+
+    pub fn version(&self) -> u32 {
+        self.inner.version
+    }
+}
+
+pub fn read<P: AsRef<Path>>(path: P) -> Result<IR> {
+    IR::load_protobuf(path)
 }
 
 #[cfg(test)]
