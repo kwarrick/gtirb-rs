13362c877ca72e66b812f0261be235e448f9bb93
Kevin Warrick <kwarrick@grammatech.com>
2021-08-11 14:11:53 -0400

    Enable tests

--- src/context.rs
+++ src/context.rs
@@ -57,38 +57,22 @@ mod tests {
         ir.context.index.borrow();
     }
 
-    // TODO: XXX: BAD! BAD!
-    // #[test]
-    // fn bad_bad_multiple_muts() {
-    //     let mut ctx = Context::new();
-    //     let mut ir = IR::new(&mut ctx);
-    //     let module = Module::new(&mut ctx, "foo");
-    //     let uuid = module.uuid();
-    //     ir.add_module(module);
+    #[test]
+    fn can_find_node_by_uuid() {
+        let mut ctx = Context::new();
+        let mut ir = IR::new(&mut ctx);
+        let module = Module::new(&mut ctx, "foo");
+        let uuid = module.uuid();
+        ir.add_module(module);
 
-    //     let mut a = ir.modules().nth(0).unwrap();
-    //     let mut a_ = ctx.find_node::<Module>(&uuid).unwrap();
+        let node = ctx.find_node::<Module>(&uuid);
+        assert!(node.is_some());
+        assert_eq!(uuid, node.unwrap().uuid());
 
-    //     a.set_name("foo");
-    //     a_.set_name("bar");
-    // }
+        let mut node = ctx.find_node::<Module>(&uuid).unwrap();
+        node.set_name("bar");
 
-    // #[test]
-    // fn can_find_node_by_uuid() {
-    //     let mut ctx = Context::new();
-    //     let mut ir = IR::new(&mut ctx);
-    //     let module = Module::new(&mut ctx, "foo");
-    //     let uuid = module.uuid();
-    //     ir.add_module(module);
-
-    //     let node = ctx.find_node::<Module>(&uuid);
-    //     assert!(node.is_some());
-    //     assert_eq!(uuid, node.unwrap().uuid());
-
-    //     let mut node = ctx.find_node::<Module>(&uuid).unwrap();
-    //     node.set_name("bar");
-
-    //     let module = ir.modules().last().unwrap();
-    //     assert_eq!(module.name(), "bar");
-    // }
+        let module = ir.modules().last().unwrap();
+        assert_eq!(*module.name(), "bar");
+    }
 }
